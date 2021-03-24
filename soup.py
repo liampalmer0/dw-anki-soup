0bbe0aa70b44f4e7e3847743e81d68e5e9133c48
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2021-03-24 18:32:19 -0400

    add click package
    start interactive add mode
    improve string gathering/parsing

--- soup.py
+++ soup.py
@@ -1,9 +1,24 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 import subprocess
+import click
 import sys
 
-soup = BeautifulSoup(subprocess.run(["curl", sys.argv[1]], capture_output=True).stdout, 'html.parser')
+@click.command()
+@click.option('-i', is_flag=True, help='Interactively review and confirm vocabulary entries')
+@click.argument('url')
+def parse(url, i):
+    only_vocab = SoupStrainer('div', class_='section exercise-container vocabulary copy')
+    soup = BeautifulSoup(subprocess.run(["curl", url], capture_output=True).stdout, 'html.parser', parse_only=only_vocab)
+    if i:
+        print('Interactive Mode')
+        for entry in soup.select("div.row.vocabulary"):
+            cols = entry.find_all('div', recursive=False)
+            if len(cols) == 3:
+                click.echo(cols[0].get_text().strip().replace('\n', ''))
+                click.echo(cols[2].get_text().strip().replace('\n', ''))
+                click.confirm('Use this entry?', abort=True)
+    else:
+        click.echo('TODO: setup auto anki add')
 
-for entry in soup.select("div.row .vocabulary-entry .richtext-content-container"):
-    for s in entry.stripped_strings:
-        print(s)
+if __name__ == '__main__':
+    parse()
