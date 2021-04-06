967781887d2b39cc975a9fcedc036246ab465fc6
liampalmer0 <49377856+liampalmer0@users.noreply.github.com>
2021-04-06 19:02:14 -0400

    add deck creation with genanki
    add genanki to convert soup into importable anki deck file

--- soup.py
+++ soup.py
@@ -5,20 +5,48 @@ import sys
 import unicodedata
 import genanki
 
+def create_deck():
+    return genanki.Deck(2017993711, 'DW Vocabulary')
+
+def create_model():
+    return genanki.Model(
+        1844773645,
+        'dw-anki model',
+        fields=[
+            {'name': 'German'},
+            {'name': 'English'}
+        ],
+        templates=[
+            {'name': 'DE to E',
+             'qfmt': '{{German}}',
+             'afmt': '{{English}}'},
+            {'name': 'E to DE',
+             'qfmt': '{{English}}',
+             'afmt': '{{German}}'}
+        ]
+    )
+
 def validate_use_entry(choice):
     if choice != 'y' and choice != 'n' and choice != 'e' and choice != 'edit':
-        click.echo(click.style('Unrecognized option. Choice must be [y]es [n]o or [e]dit', fg='red'))
+        click.echo(click.style(
+            'Unrecognized option. Choice must be [y]es [n]o or [e]dit',
+             fg='red'))
         return False
     else: 
         return True
 
-def add_entries(entries, deck):
-    # TODO: Add all to anki deck
-    click.echo('adding {} to the {} deck'.format(len(entries), deck))
+def add_entries(entries, deck, model):
+    click.echo('adding {} entries'.format(len(entries), deck))
+    for e in entries:
+        deck.add_note(genanki.Note(
+            model=model,
+            fields=[e['front'], e['back']]
+        ))
 
 def edit(**args):
     choice = click.prompt('Edit [front/back/all]')
-    while choice != 'all' and choice != 'front' and choice != 'back' and choice != 'q':
+    while (choice != 'all' and choice != 'front'
+            and choice != 'back' and choice != 'q'):
         click.echo(click.style('Invalid input! ([q] to quit editing)'))
         choice = click.prompt('Edit [front/back/all]')
     if choice == 'q':
@@ -35,24 +63,56 @@ def edit(**args):
     return new_words
 
 @click.command()
-@click.option('-i', is_flag=True, help='Interactively review and confirm vocabulary entries')
+@click.option(
+    '-i', is_flag=True,
+    help='Interactively review and confirm vocabulary entries')
 @click.argument('url')
 def parse(url, i):
-    only_vocab = SoupStrainer('div', class_='section exercise-container vocabulary copy')
-    soup = BeautifulSoup(subprocess.run(["curl", url], capture_output=True).stdout, 'html.parser', parse_only=only_vocab)
+    click.secho(
+        '\n%%%%%%%%    %%%%        %%%%            %%%%%%      %%%%%%   %'
+        '%%%  %%%%   %%%%  %%%%%%\n'
+        '%%%%  %%%%  %%%%   %%   %%%%           %%%%%%%%     %%%%%%%  %'
+        '%%%  %%%% %%%%     %%%%\n'
+        '%%%%  %%%%  %%%%  %%%%  %%%%   %&%    %%%%  %%%%    %%%%%%%% %'
+        '%%%  %%%%%%%       %%%%\n'
+        '%%%%  %%%%   %%%%%%%%%%%%%%          %%%%%%%%%%%%   %%%% %%%%%'
+        '%%%  %%%% %%%%     %%%%\n'
+        '%%%%%%%%      %%%%%  %%%%%          %%%%      %%%%  %%%%  %%%%'
+        '%%%  %%%%   %%%%  %%%%%%\n',
+        fg='blue', bold=True
+    )
+    only_vocab = SoupStrainer(
+        'div', class_='section exercise-container vocabulary copy')
+    soup = BeautifulSoup(
+        subprocess.run(["curl", url], capture_output=True).stdout,
+        'html.parser', parse_only=only_vocab)
     if i:
+        click.echo(
+            '== Interactive Mode == \n'
+            '> Review and choose which words to add\n'
+        )
         add = []
         for entry in soup.select("div.row.vocabulary"):
             cols = entry.find_all('div', recursive=False)
             if len(cols) == 3:
-                front = unicodedata.normalize('NFKC', cols[0].get_text().strip().replace('\n', ' ', 1).replace('\n', ''))
-                back = unicodedata.normalize('NFKC', cols[2].get_text().strip().replace('\n', ' ', 1).replace('\n', ''))
+                front = unicodedata.normalize(
+                    'NFKC',
+                    cols[0].get_text().strip()
+                        .replace('\n', ' ', 1)
+                        .replace('\n', ''))
+                back = unicodedata.normalize(
+                    'NFKC',
+                    cols[2].get_text().strip()
+                        .replace('\n', ' ', 1)
+                        .replace('\n', ''))
                 click.echo(click.style(front, fg='green'))
                 click.echo(click.style(back, fg='green'))
                 valid = False
                 choice = 'n'
                 while not valid:
-                    choice = click.prompt('Use this entry? [y]es/[n]o/[e]dit', default='y').lower()
+                    choice = click.prompt(
+                        'Use this entry? [y]es/[n]o/[e]dit',
+                        default='y').lower()
                     valid = validate_use_entry(choice) 
                 if choice == 'y':
                    add.append({'front': front, 'back': back})
@@ -61,8 +121,9 @@ def parse(url, i):
                 elif choice == 'e':
                     add.append(edit(front=front, back=back))
         click.echo(add)
-        add_entries(add, 'default')
-                    
+        deck = create_deck()
+        add_entries(add, deck, create_model())
+        genanki.Package(deck).write_to_file('dw-vocab.apkg')
     else:
         # TODO: set up automatic add
         click.echo('adding all to deck')
